295b75629e7790a094853fddb3ff6f950693fd68
dengyt <2248545525@qq.com>
2018-12-20 16:35:06 +0800

    抽取公共代码和第三方库代码

--- webpack.config.js
+++ webpack.config.js
@@ -108,6 +108,10 @@ if(isDev){
       new webpack.NoEmitOnErrorsPlugin()
   )
 }else{
+    config.entry ={
+        app:path.join(__dirname,'src/index.js'),
+        vendor:['vue']
+    }
     config.module.rules.push( {
         test: /\.styl(us)?$/,
         include: [path.resolve(__dirname, 'src')],
@@ -126,8 +130,36 @@ if(isDev){
     config.plugins.push(
         new MiniCssExtractPlugin({
             filename: '[name].[hash:8].css',
-            chunkFilename: '[id].[hash:8].css'
+            // chunkFilename: '[id].[hash:8].css'
         })
     )
+    config.optimization={
+        splitChunks: {
+            cacheGroups: {
+              commons: {
+                // 抽离自己写的公共代码
+                chunks: 'initial',
+                name: 'common', // 打包后的文件名，任意命名
+                minChunks: 2, //最小引用2次
+                minSize: 0 // 只要超出0字节就生成一个新包
+              },
+              styles: {
+                name: 'styles', // 抽离公用样式
+                test: /\.css$/,
+                chunks: 'all',
+                minChunks: 2,
+                enforce: true
+              },
+              vendor: {
+                // 抽离第三方插件
+                test: /node_modules/, // 指定是node_modules下的第三方包
+                chunks: 'initial',
+                name: 'vendor', // 打包后的文件名，任意命名
+                // 设置优先级，防止和自定义的公共代码提取时被覆盖，不进行打包
+                priority: 10
+              }
+            }
+          }
+    }
 }
 module.exports = config
