64432cdd56d8adf1ffd84df93b8574a64bc3ff5c
dengyt <2248545525@qq.com>
2019-01-02 11:19:00 +0800

    分离工程

---
+++ build/webpack.config.base.js
@@ -0,0 +1,67 @@
+// 共同配置，webpack基础配置，所有环境都用到
+const path = require('path');
+
+const config ={
+    target:'web',
+    entry:path.join(__dirname,'../src/index.js'),
+    output:{
+        filename:'bundle.[hash:8].js',
+        path:path.join(__dirname,'../dist')
+    },
+    module:{
+        rules:[
+            {
+                test:/\.vue$/,
+                loader:'vue-loader'
+            },
+            // {
+            //     test:/\.css$/,
+            //     // loader:'css-loader'//处理css文件
+            //     use:[
+            //         MiniCssExtractPlugin.loader,
+            //         'css-loader',
+            //         {
+            //             loader: 'postcss-loader',
+            //             options: {
+            //                 plugins: [require('autoprefixer')]
+            //             }
+            //         }
+            //     ]
+            // },
+            {
+                test:/\.(gif|jpg|jpeg|png|svg)$/,
+                use:[
+                    {
+                        loader:'url-loader',
+                        options:{//指定loader的一些操作方式
+                            limit:1024,
+                            name:'resources/[path][name].[hash:8].[ext]'//最后输出文件的名字
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: ['@babel/preset-env'],
+                    plugins: ['@babel/plugin-proposal-object-rest-spread']
+                  }
+                }
+            },
+            {
+                test: /.jsx$/, //使用loader的目标文件。这里是.jsx
+                loader: 'babel-loader'
+            },
+            {
+                test: /.js$/, //使用loader的目标文件。这里是.jsx
+                loader: 'babel-loader',
+                exclude:/node_modules/
+            }            
+        ]
+    }
+}
+
+module.exports = config
---
+++ build/webpack.config.client.js
@@ -0,0 +1,98 @@
+const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+const HTMLPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.config.base');
+
+//  判断开发环境还是生产环境
+const isDev = process.env.NODE_ENV === 'development'
+
+let config
+
+const devServer ={
+    port:8000,
+    host:'0.0.0.0',//同局域网可以访问
+    overlay:{
+        errors:true
+    },
+    open:true,
+    hot:true
+  //   historyFallback:{
+
+  //   }
+}
+const defaultPlugins=[
+    new webpack.DefinePlugin({
+        'process.env':{
+            NODE_ENV:isDev?'"development"':'"production"'
+        }
+    }),
+    new VueLoaderPlugin(),
+    new HTMLPlugin()
+]
+if(isDev){
+  config = merge(baseConfig,{
+      devtool:'#cheap-module-eval-source-map' ,
+      module:{
+          rules:[
+            {
+                test: /\.styl(us)?$/,
+                use:[
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader:'postcss-loader',
+                        options:{
+                            sourceMap:true
+                        } 
+                    },
+                    'stylus-loader'
+                ]
+              }
+          ]
+      },
+      devServer:devServer,
+      plugins:defaultPlugins.concat([
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin()
+    ])
+  })
+}else{
+    config = merge(baseConfig, {
+        entry: {
+          app: path.join(__dirname, '../src/index.js'),
+          vendor: ['vue']
+        },
+        output: {
+          filename: '[name].[chunkhash:8].js'
+        },
+        module: {
+          rules: [
+            {
+                test: /\.styl(us)?$/,
+                include: [path.resolve(__dirname, '../src')],
+                use: [
+                  MiniCssExtractPlugin.loader,
+                  'css-loader',
+                  {
+                    loader: 'postcss-loader',
+                    options: {
+                      plugins: [require('autoprefixer')]
+                    }
+                  },
+                  'stylus-loader'
+                ]
+            }
+          ]
+        },
+        plugins: defaultPlugins.concat([
+            new MiniCssExtractPlugin({
+                filename: '[name].[hash:8].css',
+                // chunkFilename: '[id].[hash:8].css'
+            })
+        ])
+      })
+}
+module.exports = config
