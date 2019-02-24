34d3033278bd7bef24bbc75f79ec19a64b58da38
dengyt <2248545525@qq.com>
2019-02-24 17:53:43 +0800

    new 路由的时候配置项

--- client/config/router.js
+++ client/config/router.js
@@ -5,10 +5,24 @@ import routes from './routes'
 export default ()=>{
     return new VueRouter({
         routes,
-        mode:'history'
+        mode:'history',
         // base:'/base/'//链接加上base前缀，base不是强制的
         // linkActiveClass:'active-link',//部分匹配
         // linkExactActiveClass:'exact-active-link'//完全匹配
-        
+        scrollBehavior (to, from, savedPosition) {
+            // return 期望滚动到哪个的位置
+            if(savedPosition){
+                return savedPosition//如果页面之前有访问过，那么下次进入页面的时候回到之前访问滚动条的位置
+            }else{
+                return { x: 0, y: 0 }
+            }
+        },
+        // parseQuery(){
+
+        // },
+        // stringifyQuery(){
+            
+        // }
+        fallback:true
     })
 };
