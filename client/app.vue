6e556f21e1669a88fcc62caa3c16d05f86d40bf2
dengyt <2248545525@qq.com>
2019-02-25 14:11:51 +0800

    vuex 模块

--- client/app.vue
+++ client/app.vue
@@ -3,6 +3,7 @@
         <div id="cover"></div>
         <Header></Header>  
         <p>{{fullName}}{{count}}</p>
+        <p>{{textA}}{{textPlus}}</p>
         <!-- <Todo></Todo> -->
         <!-- 挂载路由 -->
         <router-link to='./app'>app</router-link>
@@ -16,8 +17,10 @@
 <script>
 // 快速在组件内使用vuex
 import {
-  mapState
-  // mapGetters
+  mapState,
+  mapGetters,
+  mapActions,
+  mapMutations
 } from 'vuex'
 import Header from './layout/header.vue'
 import Footer from './layout/footer.jsx'
@@ -32,21 +35,44 @@ export default {
         return{
         }
     },
+    methods:{
+      ...mapActions(['updateCountAsync','a/add','textAction']),
+      ...mapMutations(['updateCount','a/updateText'])
+    },
     mounted(){
       console.log(this.$store)
-      let i = 1
-      setInterval(()=>{
-        this.$store.commit('updateCount',i++)
-      },1000)
+      // let i = 1
+      // this.$store.state.count = 3;//在vuex中规定了strict为true，在开发环境中就不允许这样做，浏览器控制台会报错
+      // setInterval(()=>{
+      //   this.$store.commit('updateCount',i++)
+      // },1000)
+      // this.$store.dispatch('updateCountAsync',{ num:5,time:2000 })
+      this.updateCountAsync({ num:5,time:2000 })//辅助函数用法
+      this['a/updateText']('123')//a模块化下面的mutation方法
+      this['a/add']()
+      this.textAction()
     },
     computed:{
-      ...mapState(['count']),
+      // textA(){
+      //   return this.$store.state.a.text
+      // },
+      textB(){
+        return this.$store.state.b.text
+      },
+      ...mapState({
+        count:(state)=>state.count,
+        textA:state => state.a.text
+      }),
       // count(){
       //   return this.$store.state.count
       // },
-      fullName(){
-        return this.$store.getters.fullName
-      }
+      // fullName(){
+      //   return this.$store.getters.fullName
+      // }
+      ...mapGetters({
+        fullName:'fullName',
+        textPlus:'a/textPlus'
+      })
     }
 }
 </script>
--- client/store/store.js
+++ client/store/store.js
@@ -2,11 +2,51 @@ import Vuex from 'vuex'
 import defaultState from './state/state'
 import mutations from './mutations/mutations'
 import getters from './getters/getters'
+import actions from './actions/actions'
+
+const isDev = process.env.NODE_ENV === 'development'
 
 export default ()=>{
     return new Vuex.Store({
+        strict:isDev,//在开发环境中，在正式环境中关掉
         state:defaultState,
-        mutations:mutations,
-        getters:getters
+        mutations,//修改一个数据，放在mutations,mutations是同步的
+        getters,
+        actions,
+        modules:{//模块下还可以增加模块
+            a:{
+                namespaced: true,
+                state: {
+                    text:1
+                },
+                mutations: {//默认生成的mutation是在全局的命名空间下面的，如果想在只在a模块下，需要加上namespaced
+                    updateText(state,text){
+                        console.log(state)
+                        state.text=text
+                    }
+                },
+                getters:{
+                    textPlus(state,getters,rootState){
+                        return state.text +rootState.b.text//拿到全局state的方法
+                    }
+                },
+                actions:{
+                    add({ state,commit,rootState }){
+                        // commit('updateText',rootState.count)//直接调用是是当前作用域下面的mutation
+                        commit('updateCount',rootState.count,{ root:true })
+                    }
+                }
+            },
+            b:{
+                state: {
+                    text: 2
+                },
+                actions:{
+                    textAction({ commit }){
+                        commit('a/updateText','test test')
+                    }
+                }
+            }
+        }
     })
 }
