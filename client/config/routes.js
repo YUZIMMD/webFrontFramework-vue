db6fc814590f790eb31c87d042252025611c6b85
dengyt <2248545525@qq.com>
2019-02-26 17:02:14 +0800

    命名理由

--- client/config/routes.js
+++ client/config/routes.js
@@ -8,19 +8,25 @@ export default [
         redirect:'/app'
     },
     {
-        path:'/app/:id',
-        component:Todo,
+        path:'/app',
+        components:{
+            default:Todo,
+            a:Login
+        },
         name:'app',
-        // children:[
-        //     {
-        //         path:'/test',//如果在hash模式下，设置成test，因为hash模式下根路径不会变，所以能够正常渲染，但是如果设置成history模式，那么就会变成app/test，页面会报404的错误，这个时候在路径前面加上/就指向了根路径，但是访问的时候就是localhost:8000/test
-        //         component:Login
-        //     }
-        // ]
+        children:[
+            {
+                path:'test',//如果在hash模式下，设置成test，因为hash模式下根路径不会变，所以能够正常渲染，但是如果设置成history模式，那么就会变成app/test，页面会报404的错误，这个时候在路径前面加上/就指向了根路径，但是访问的时候就是localhost:8000/test
+                component:Login
+            }
+        ]
     },
     {
         path:'/login',
-        component:Login,
+        components:{
+            default:Login,
+            a:Todo
+        },
         name:'login'
     }
 ]
